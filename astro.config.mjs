1818037eaa9cd9a1ae3347ed18e9d120d1a9a2b2
unknown <jeffreznichenko@gmail.com>
2025-07-06 23:04:19 -0400

    Initial commit 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import rehypePrettyCode from 'rehype-pretty-code';
 import { siteConfig } from './src/config';
-
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: siteConfig.site,
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind(), sitemap(), react()],
   markdown: {
     rehypePlugins: [
       [rehypePrettyCode, {
